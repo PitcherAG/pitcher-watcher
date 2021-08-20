4be5fb793cf3b4146f9df2ee0630170ea2f14e2b
Onur Elibol <onur.elibol@pitcher.com>
2021-08-20 11:04:26 +0200

    chore: remove unused

--- tests/config/jest.snapshotResolver.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  resolveSnapshotPath: (testPath, snapshotExtension) =>
-    testPath.replace('unit', 'unit/__snapshots__') + snapshotExtension,
-  resolveTestPath: (snapshotFilePath, snapshotExtension) =>
-    snapshotFilePath.replace('unit/__snapshots__', 'unit').slice(0, -snapshotExtension.length),
-  testPathForConsistencyCheck: '',
-}
