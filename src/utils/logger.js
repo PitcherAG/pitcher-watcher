40c3d07e11038cd142a19a4fcec4e659d05ae07b
Onur Elibol <onur.elibol@pitcher.com>
2021-08-20 16:36:42 +0200

    chore: update logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 
-const log = (msg, color = 'white') => console.log(chalk[color](msg))
+const log = (msg, color = 'cyan') => console.log(chalk[color](msg))
 const error = (msg, color = 'red') => console.log(chalk[color](msg))
 const warn = (msg, color = 'yellow') => console.log(chalk[color](msg))
 
