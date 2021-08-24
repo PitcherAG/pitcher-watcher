d972ba52379d5b5a344778f0be431d2fd8c62cfa
Onur Elibol <onur.elibol@pitcher.com>
2021-08-24 15:18:35 +0200

    chore: update warn message

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 
 const log = (msg, color = 'cyan') => console.log(chalk.white(`[@pitcher/watcher]:`, chalk[color](msg)))
 const error = (msg, color = 'red') => console.log(chalk[color](msg))
-const warn = (msg, color = 'yellow') => console.log(chalk[color](msg))
+const warn = (msg, color = 'yellow') => console.log(chalk.yellow(`[@pitcher/watcher]:`, chalk[color](msg)))
 
 module.exports = {
   log,
