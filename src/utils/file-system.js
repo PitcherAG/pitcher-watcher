6b602b9136e93ff2d80b5111f30e8110a19c85c3
Onur Elibol <onur.elibol@pitcher.com>
2021-08-24 18:29:50 +0200

    chore: file system logging updates

--- src/utils/file-system.js
+++ src/utils/file-system.js
@@ -10,8 +10,8 @@ const fixPath = (path) => path.replace(/(\[|\]|\s)/g, '\\$&')
 
 // cleaning with rm -rf
 // needs to be from bash because of the file system access rights on windows side
-const cleanDirectory = (path) => {
-  let destination = fixPath(/(\[|\]|\s)/g, '\\$&')
+const cleanDirectory = (path, showMessage = true) => {
+  let destination = fixPath(path)
 
   // add trailing slash, needed for rm to only clean directory
   destination += !destination.endsWith('/') ? '/*' : '*'
@@ -22,7 +22,10 @@ const cleanDirectory = (path) => {
     return
   }
 
-  log('Directory is not empty, cleaning...')
+  if (showMessage) {
+    log('Directory is not empty, cleaning...')
+  }
+
   exec(`rm -rf ${destination}`, (err) => {
     if (err) {
       error(`Something went wrong while cleaning the directory: ${destination}`)
@@ -32,13 +35,21 @@ const cleanDirectory = (path) => {
   })
 }
 
-const copyDirectory = (path) => {
-
+const bashCopy = (sources, dest) => {
+  sources.forEach((src) => {
+    exec(`cp -R ${src} ${fixPath(dest)}`, (err) => {
+      if (err) {
+        error(`Something went wrong while cleaning the directory: ${dest}`)
+        error(`${JSON.stringify(err)}`)
+        process.exit(1)
+      }
+    })
+  })
 }
 
 module.exports = {
   isDirEmpty,
   dirExist,
   cleanDirectory,
-  copyDirectory,
+  bashCopy,
 }
