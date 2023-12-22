f6594a5d120671e5b18d6db0c906f186176eba1b
Furkan Yanteri <59139373+furkanYanteri1@users.noreply.github.com>
2023-12-22 11:10:02 +0300

    fix: remove guide interval too
    Co-authored-by: Wojciech Urbański <wojtiku@gmail.com>

--- src/hmr/hmr-helper.js
+++ src/hmr/hmr-helper.js
@@ -33,7 +33,7 @@ function clearIntervals(){
       const interval_id = window.setInterval(function(){}, Number.MAX_SAFE_INTEGER);
 
       // Clear any timeout/interval up to that id
-      for (let i = 1; i < interval_id; i++) {
+      for (let i = 1; i <= interval_id; i++) {
         window.clearInterval(i);
       }
 }
